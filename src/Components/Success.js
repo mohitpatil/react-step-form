7565c78f45cf9a7f8f82b27955444f7aaee923cb
Mohit <mohit.patil@terafinainc.com>
2019-06-02 17:46:00 -0700

    adding success component

--- src/Components/Success.js
+++ src/Components/Success.js
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
 
 class Success extends Component {
   render() {
-    return <div />;
+    return (
+      <MuiThemeProvider>
+        <React.Fragment>
+          <AppBar title="Wo ho!!" showMenuIconButton={false} />
+          <h1> Thank you!!</h1>
+          <h2>Your data has been submitted successfully !!</h2>
+        </React.Fragment>
+      </MuiThemeProvider>
+    );
   }
 }
 
